b1add4d3a227a4cd7695c2e7a17733dc3ff11a78
bernardes7 <bernardes7@gmail.com>
2022-11-30 12:42:07 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,103 +16,117 @@ p5.disableFriendlyErrors = true; // disables FES
 
 function preload() {
   birds = loadSound("assets/bird.ogg");
-  chord = loadSound("assets/chord.ogg");
   drops = loadSound("assets/drops.ogg");
   leaves = loadSound("assets/leaves.ogg");
   dog = loadSound("assets/dog.ogg");
   thunder = loadSound("assets/thunder.ogg");
   glass = loadSound("assets/glass.ogg");
   cricket = loadSound("assets/cricket.ogg");
+  fly = loadSound("assets/fly.ogg");
+  seagull = loadSound("assets/seagull.ogg");
+    wolf = loadSound("assets/wolf.ogg");
+    sea = loadSound("assets/sea.ogg");
 }
 
 function setup() {
-  frameRate(3);
+  frameRate(4);
   createCanvas(windowWidth, windowHeight);
   background("white");
   textAlign(CENTER);
   noFill();
   textSize(400);
   stroke("black");
-  fill(0,0,0,100);
+  noFill();
   text("simb", windowWidth / 2, windowHeight / 3);
   text("iose", windowWidth / 2, windowHeight);
-  fill(255,0,0,180);
+  fill(255, 0, 0, 200);
   noStroke();
   circle(windowWidth / 2, windowHeight / 2, windowWidth / 1.5);
   stroke("white");
   textAlign(CENTER);
   textSize(50);
-  noFill()
-  text('tap to', windowWidth / 2, (windowHeight / 2) - 50);
+  noFill();
+  text("tap to", windowWidth / 2, windowHeight / 2 - 50);
   textSize(100);
   noFill();
-  text('start', windowWidth / 2, (windowHeight / 2) + 50);
+  text("start", windowWidth / 2, windowHeight / 2 + 50);
 
   // audio constructors
   wind = new p5.Noise();
   wind.disconnect();
   wind.start();
   filtr = new p5.BandPass();
-  reverb = new p5.Reverb();
-  
-  glass.playMode('sustain');
-
+  glass.playMode("sustain");
 }
-  
+
 function draw() {
   accel = abs(accelerationY);
   nOff = nOff + nVel;
-  filtrFreq = map(noise(nOff), 0, 1, fmin, fmax); //
+  filtrFreq = map(noise(nOff), 0, 1, fmin, fmax);
   filtr.set(filtrFreq, filtrWidth);
 
   // Spring
   if (playing === 1 && option === 1) {
-    if(drops.isPlaying() === false){drops.loop();
-    reverb.process(drops,3,1);}
+    if (drops.isPlaying() === false) {
+      drops.play(0, random(0.8, 1.2), 0, 0);
+    }
     drops.rate(map(noise(nOff), 0, 1, 0.4, 1.3));
     if (birds.isPlaying() === false) {
       birds.play();
-  }
-    if(random(100) < 8) {
-    cricket.play(0, random(0.5, 2), 0.6, 0);
     }
-      birds.rate(map(noise(nOff), 0, 1, 0.2, 1.7));
-    let vol = min(accel / 60, 1);
-    print(vol);
+    if (random(100) < 5) {
+      cricket.play(0, random(0.6, 2), 0.5, 0);
+    }
+    birds.rate(map(noise(nOff), 0, 1, 0.2, 1.7));
+    let vol = min(accel / 40, 1);
     birds.setVolume(vol);
   }
-  
-    // Summer
-  if (playing === 1 && option === 2){ 
-    let rand = random(100); 
-    if(rand < 10) {
-    let tones = [0.35,0.4,0.45,0.5,0.55];
-    glass.play(0, tones[int(random(tones.length))], 0.1, 0);
+
+  // Summer
+  if (playing === 1 && option === 2) {
+    let rand = random(100);
+    if (sea.isPlaying() === false) {
+      sea.play();
     }
+    if (rand < 10) {
+      let tones = [0.35, 0.4, 0.45, 0.5, 0.55];
+      glass.play(0, tones[int(random(tones.length))], 0.4, 0);
+    }
+    if (fly.isPlaying() === false && random(100) < 5) {
+    fly.play(0, random(0.8, 1.2), 0.4, 0);
+    }
+    if (seagull.isPlaying() === false) {
+      seagull.play();
+    }    
+    let vol = min(accel / 40, 1);
+    seagull.setVolume(vol);
   }
 
-     // Autumn
+  // Autumn
   if (playing === 1 && option === 3) {
     if (leaves.isPlaying() === false && random(100) < 10) {
       leaves.play(0, 1, 0.6, random(2));
     }
     leaves.rate(map(noise(nOff), 0, 1, 0.6, 1));
-   
+
     if (dog.isPlaying() === false && random(100) < 5) {
       dog.play(0, 1, 0.6, random(0.2));
-
     }
     dog.rate(map(noise(nOff), 0, 1, 0.4, 1));
-  } 
+  }
 
   //winter
   if (playing === 1 && option === 4) {
-    if (drops.isPlaying() === false && second() % 5 === 0) {
-    drops.play();
-    reverb.process(drops,3,1);
-    setTimeout(drops.play(), 2500);
+    let rand = random(100); 
+    if (wolf.isPlaying() === false && rand < 4) {
+      wolf.play(0, random(0.8, 1), 0.7, random(1.5));
     }
-    if(accel>20 && thunder.isPlaying() === false){thunder.play(0,random(0.5,1.5),0.5,random(1))}
+    if (drops.isPlaying() === false) {
+      drops.play();
+    }
+    if (accel > 20 && thunder.isPlaying() === false) {
+      thunder.play(0, random(0.5, 1.5), 0.5, random(1));
+    }
     drops.rate(map(noise(nOff), 0, 1, 0.4, 1.3));
   }
 }
@@ -124,72 +138,73 @@ function playRain(start, min, max, w, v) {
     fmin = min;
     fmax = max;
     filtr.process(wind);
-    filtr.disconnect();
+    filtr.connect();
     filtr.gain(0.5);
-    reverb.connect();
-
-    // 3 second reverbTime, decayRate of 2%
-    reverb.drywet(0.8);
-    reverb.process(filtr, 3, 2);
   } else {
-    reverb.disconnect();
+    filtr.disconnect();
   }
 }
 
 function run() {
+  clear();
   playing = 1;
   createCanvas(windowWidth, windowHeight);
+   
 
-  noFill('darkgrey');
-  stroke('black');
+  noFill("darkgrey");
+  stroke("black");
 
-  
   fill(colorSpring);
   rect(0, 0, windowWidth / 2, windowHeight / 2);
   textAlign(LEFT);
   textSize(40);
-  text('spring', 10, 50);
+  text("spring", 10, 50);
 
   fill(colorSummer);
   rect(windowWidth / 2, 0, windowWidth - 10, windowHeight / 2);
-  text('summer', (windowWidth / 2) + 10, 50);
+  text("summer", windowWidth / 2 + 10, 50);
 
   fill(colorAutumn);
   rect(0, windowHeight / 2, windowWidth / 2, windowHeight - 10);
-  text('autumn', 10, (windowHeight/2)+50);
+  text("autumn", 10, windowHeight / 2 + 50);
 
   fill(colorWinter);
   rect(windowWidth / 2, windowHeight / 2, windowWidth - 1, windowHeight - 10);
-  text('winter', (windowWidth / 2) + 10, (windowHeight/2)+50);  
+  text("winter", windowWidth / 2 + 10, windowHeight / 2 + 50);
 }
 
 function stop() {
   playRain(0);
   birds.stop();
-  chord.stop();
   drops.stop();
   leaves.stop();
   dog.stop();
   cricket.stop();
   glass.stop();
+  fly.stop();
+  seagull.stop();
+  wolf.stop();
+  sea.stop(); 
 }
 
 function mousePressed() {
   if (!playing) {
     fullscreen(1);
-    background('white');
-    stroke('black');
+    createCanvas(windowWidth, windowHeight);
+    background("white");
+    stroke("black");
     textSize(50);
-    text('double tap', windowWidth/2, windowHeight/2 -100); 
-    text('the seasons',windowWidth/2, windowHeight/2 - 50); 
-    text('to activate', windowWidth/2, windowHeight/2);
+    text("double tap", windowWidth / 2, windowHeight / 2 - 100);
+    text("the seasons", windowWidth / 2, windowHeight / 2 - 50);
+    text("to activate", windowWidth / 2, windowHeight / 2);
     setTimeout(run, 2000);
   }
 }
 
 function doubleClicked() {
   if (playing) {
-   // Spring
+    
+    // Spring
     if (
       mouseX > 0 &&
       mouseX < windowWidth / 2 &&
@@ -247,7 +262,7 @@ function doubleClicked() {
         colorSummer = "white";
         colorAutumn = "red";
         colorWinter = "white";
-        playRain(1,200, 800, 3, 1);
+        playRain(1, 400, 800, 3, 0.8);
       } else {
         option = 0;
         colorAutumn = "white";
@@ -269,7 +284,7 @@ function doubleClicked() {
         colorSummer = "white";
         colorAutumn = "white";
         colorWinter = "red";
-        playRain(1, 400, 1200, 1, 1);
+        playRain(1, 400, 1000, 3, 0.8);
       } else {
         option = 0;
         colorWinter = "white";
